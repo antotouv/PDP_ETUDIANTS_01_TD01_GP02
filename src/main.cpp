109d1adb2f5fdaa5291cd4cb3aef85e3601463ac
Canttax <timfrteam0@gmail.com>
2026-01-07 11:15:00 +0100

    etape_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+// Define the pins that we will use
+#define CAPTEUR 33
+#define LED 26
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // testtt
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(200);
 }
